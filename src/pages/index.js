93ee808639907d225769ca77cfc9d35104b7c171
Johnnie Regalado <johnniereg@gmail.com>
2022-04-30 10:12:03 -0400

    Try another test form

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,34 +73,17 @@ const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=de
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <form name="contact" method="POST" data-netlify="true">
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{" "}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+    <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
     </form>
   </Layout>
 )
