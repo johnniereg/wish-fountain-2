6a10cad0e9c6ddbacfa83cfce18c99beaa2897ff
Johnnie Regalado <johnniereg@gmail.com>
2022-05-19 21:16:17 -0400

    Test  handling different devices for dnd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react"
-
+import { isMobile } from "react-device-detect"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
+import { TouchBackend } from "react-dnd-touch-backend"
 
 import Layout from "../components/layout"
 import App from "../components/app"
@@ -9,6 +10,15 @@ import App from "../components/app"
 import "../styles/index.css"
 
 const IndexPage = () => {
+  if (isMobile) {
+    return (
+      <DndProvider backend={TouchBackend}>
+        <Layout>
+          <App />
+        </Layout>
+      </DndProvider>
+    )
+  }
   return (
     <DndProvider backend={HTML5Backend}>
       <Layout>
