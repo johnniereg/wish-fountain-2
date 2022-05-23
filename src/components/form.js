1bb14dc636b74e840bb59b98abc1b230b1957bb8
Johnnie Regalado <johnniereg@gmail.com>
2022-05-23 15:35:40 -0400

    Get wishes API working

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,4 @@
-import * as React from "react"
-import { useState } from "react"
+import React, { useState } from "react"
 import { navigate } from "gatsby-link"
 
 import "../styles/components/form.scss"
--- src/components/wishes.js
+++ src/components/wishes.js
@@ -4,32 +4,37 @@ import { useState, useEffect } from "react"
 import "../styles/components/wishes.scss"
 
 const Wishes = () => {
-  const [wishes, setWishes] = useState(["test", "test2"])
+  const [wishes, setWishes] = useState([])
 
   useEffect(() => {
-    fetch(
-      "https://api.netlify.com/api/v1/sites/94286650-7f42-4876-a795-167e52313b8f/forms",
-      {
-        method: "GET",
-        headers: {
-          "User-Agent": "Wish Fountain (johnniereg@gmail.com)",
-          Authorization: "Bearer fz2f7iyTqw5TI-jkshSWuT8_DdCLYUpqQdK08KrBSFs",
-        },
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.netlify.com/api/v1/sites/94286650-7f42-4876-a795-167e52313b8f/forms/628bd98d7a3f3700087883c5/submissions",
+          {
+            method: "GET",
+            headers: {
+              "User-Agent": "Wish Fountain (johnniereg@gmail.com)",
+              Authorization:
+                "Bearer fz2f7iyTqw5TI-jkshSWuT8_DdCLYUpqQdK08KrBSFs",
+            },
+          }
+        )
+        const json = await response.json()
+        console.log("json", json)
+        setWishes(json)
+      } catch (error) {
+        console.log("Error: ", error)
       }
-    )
-      .then(response => {
-        console.log(response, "response")
-        response.json()
-      })
-      .then(response => {
-        if (response) setWishes(response)
-      })
+    }
+    fetchData()
   }, [])
 
   return (
     <div className="wishes">
-      {wishes.forEach(wish => {
-        return <div key={wish.id}>{wish}</div>
+      <div>Wishes</div>
+      {wishes.map(wish => {
+        return <div key={wish.number}>{wish.title}</div>
       })}
     </div>
   )
