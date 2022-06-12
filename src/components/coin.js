e70b7aca79eb332349660b76794b743d0681a78a
Johnnie Regalado <johnniereg@gmail.com>
2022-06-12 12:31:57 -0400

    position gems

--- src/components/coin.js
+++ src/components/coin.js
@@ -44,13 +44,7 @@ const Coin = ({ image, isDragging, name, toggleForm, x, y }) => {
         onDragEnd={onDragEnd}
         targetKey="well"
       >
-        <img
-          alt={name}
-          className="icon"
-          style={{ cursor: "move", maxHeight: "75px", width: "auto" }}
-          src={image}
-          draggable={false}
-        ></img>
+        <img alt={name} className="icon" src={image} draggable={false}></img>
       </DragDropContainer>
     </div>
   )
