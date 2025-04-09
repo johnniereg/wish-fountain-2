13ae516e0364b4199c60bc2d311fc870735cfb1e
Johnnie Regalado <johnniereg@gmail.com>
2025-04-09 13:01:39 -0400

    Update title image

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,14 +2,13 @@ import * as React from "react"
 
 import "../styles/components/hero.scss"
 
-import Title from "../images/title.gif"
+import Title from "../images/title.png"
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="image__wrapper">
         <img alt="Wish Fountain" className="hero__image" src={Title}></img>
-        <h1>Wish Fountain Art City</h1>
       </div>
     </div>
   )
