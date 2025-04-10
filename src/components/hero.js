8146f93f127edd77792967f960af92f57600b637
Johnnie Regalado <johnniereg@gmail.com>
2025-04-10 10:31:06 -0400

    More asset updates

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,12 +3,15 @@ import * as React from "react"
 import "../styles/components/hero.scss"
 
 import Title from "../images/title.png"
+import TitleMobile from "../images/title-mobile.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="image__wrapper">
-        <img alt="Wish Fountain" className="hero__image" src={Title}></img>
+        <img alt="Wish Fountain" className="hero__image hero__image--desktop" src={Title}></img>
+        <img alt="Wish Fountain" className="hero__image hero__image--mobile" src={TitleMobile}></img>
+
       </div>
     </div>
   )
